4812755ea703b9296f427240a1b3c1a9339c942c
DMacharia-887 <denniskahiga92@gmail.com>
2025-03-25 20:06:27 +0300

    Add files via upload

---
+++ apt2080 if-exercise.cs
@@ -0,0 +1,40 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int daysUntilExpiration = random.Next(12);
+        int discountPercentage = 0;
+
+        if (daysUntilExpiration == 1) 
+        {
+            discountPercentage = 20;
+        }
+        else if (daysUntilExpiration <= 5) 
+        {
+            discountPercentage = 10;
+        }
+
+        if (daysUntilExpiration < 1) 
+        {
+            Console.WriteLine("Your subscription has expired!");
+        }
+        else if (daysUntilExpiration == 1) 
+        {
+            Console.WriteLine("Your subscription expires within a day!");
+            Console.WriteLine($"Renew now and save {discountPercentage}%!");
+        }
+        else if (daysUntilExpiration <= 5) 
+        {
+            Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+            Console.WriteLine($"Renew now and save {discountPercentage}%!");
+        }
+        else
+        {
+            Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+            Console.WriteLine("Your subscription will expire soon. Renew now!");
+        }
+    }
+}
